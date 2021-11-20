57d0c56aa5e0cc1b8ebe324bf7171f8e03a28e67
umangx <agent.u99pro@gmail.com>
2021-11-20 16:13:03 +0530

    add the find function for creating the history.txt

--- run.py
+++ run.py
@@ -1,14 +1,20 @@
 import os; 
 import sys
 
-term = sys.argv[1]; 
+location = sys.argv[1];
+term = sys.argv[2]; 
+os.system("find " + location +" -type f > history.txt"); 
 
 file1 = open('history.txt', 'r')
 Lines = file1.readlines() 
 
-# Strips the newline character
+def execute_locator(file):    
+    data = os.popen("cat " + file +" | python3 ~/dev/crawler/test2.py " + term ).read(); 
+    if data != 0:   
+       print(file + " --> " + data);
+
 for line in Lines:
     file = line.strip();
-    data = os.popen("cat " + file +" | python3 ~/dev/crawler/test2.py " + term ).read();
-    if data != 0:   
-       print(file + " --> " + data); 
+    execute_locator(file); 
+
+os.system("rm history.txt")       
