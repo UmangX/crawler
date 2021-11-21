9e496589b35fe6cba0b7c1088fd61552feda0315
Umang Patel <69800542+UmangX@users.noreply.github.com>
2021-11-21 15:14:49 +0530

    added character limitation

--- run.py
+++ run.py
@@ -1,20 +1,29 @@
-import os; 
+import os;
 import sys
 
 location = sys.argv[1];
-term = sys.argv[2]; 
-os.system("find " + location +" -type f > history.txt"); 
+term = sys.argv[2];
+
+if len(term) < 5:
+   print("Search term too small try with more then 5 character")
+   exit();
+
+os.system("find " + location +" -type f > history.txt");
+
+file_viewed = 0;
 
 file1 = open('history.txt', 'r')
-Lines = file1.readlines() 
+Lines = file1.readlines()
 
-def execute_locator(file):    
-    data = os.popen("cat " + file +" | python3 ~/dev/crawler/test2.py " + term ).read(); 
-    if data != 0:   
-       print(file + " --> " + data);
+def execute_locator(file):
+    data = os.popen("cat " + file +" | python3 ~/dev/crawler/test2.py " + term ).read();
+    print(file + " --> " + data);
 
 for line in Lines:
     file = line.strip();
-    execute_locator(file); 
-
-os.system("rm history.txt")       
+    execute_locator(file);
+    file_viewed +=1;
+    
+print("------------------------------------------------------------------")
+print("Read",file_viewed,"Files In",location)
+os.system("rm history.txt")
