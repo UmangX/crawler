e3bbce19b4e10c94c959d300737c6151e9fc1522
Umang Patel <69800542+UmangX@users.noreply.github.com>
2021-11-28 11:35:25 +0530

    removed error msg
    now only returns the read file detials

--- run.py
+++ run.py
@@ -1,29 +1,29 @@
 import os;
 import sys
-
 location = sys.argv[1];
 term = sys.argv[2];
 
+
+test_storage = [];
+
 if len(term) < 5:
-   print("Search term too small try with more then 5 character")
+   print("Search term too small try with more then 5 char")
    exit();
 
 os.system("find " + location +" -type f > history.txt");
-
-file_viewed = 0;
-
 file1 = open('history.txt', 'r')
 Lines = file1.readlines()
 
 def execute_locator(file):
     data = os.popen("cat " + file +" | python3 ~/dev/crawler/test2.py " + term ).read();
-    print(file + " --> " + data);
+    data = data.strip()
+    if data != "Can't read the file\n0":
+       test_storage.append(data);
+       print(file + " --> " + data);
 
 for line in Lines:
     file = line.strip();
     execute_locator(file);
-    file_viewed +=1;
-    
-print("------------------------------------------------------------------")
-print("Read",file_viewed,"Files In",location)
+
 os.system("rm history.txt")
+#print(test_storage);
